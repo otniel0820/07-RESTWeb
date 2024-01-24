e6652fb958302160a39cefacef9cf586be1c05e4
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-24 02:40:09 +0100

    different req

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import http from 'http'
+import fs from 'fs'
 
 
 const server = http.createServer((req, res)=>{
 
     console.log(req.url);
 
-    res.write('hola mundo')
-    res.end()
+    // res.writeHead(200, {'Content-Type': 'text/html'})
+    // res.write(`<h1>URL ${req.url}</h1>`)
+    // res.end()
+
+    // const data = { name: 'Otniel Lascano', age: 30, city: 'Madrid'}
+    // res.writeHead(200, {'Content-Type': 'application/json'})
+    // res.end(JSON.stringify(data))
+
+    if (req.url === '/'){
+        const htmFile = fs.readFileSync('./public/index.html', 'utf-8')
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.end(htmFile)
+    }else{
+        res.writeHead(404,{'Content-Type': 'text/html'})
+        res.end()
+    }
     
 })
 
