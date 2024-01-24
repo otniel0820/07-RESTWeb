f3cddc922783bd9cc5238d520cee3ea829be7f64
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-24 02:18:29 +0100

    req and res server

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import http from 'http'
+
+
+const server = http.createServer((req, res)=>{
+
+    console.log(req.url);
+
+    res.write('hola mundo')
+    res.end()
+    
+})
+
+
+server.listen(8080, ()=>{
+    console.log('Server running on port 8080');
+    
+})
