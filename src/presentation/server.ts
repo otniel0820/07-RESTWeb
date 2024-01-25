b518a3f29e2eb18e7a1c45aa56b48a30e8f9df55
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-25 04:08:54 +0100

    serving SPA with router

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 
 
 export class Server{
@@ -14,6 +15,12 @@ export class Server{
         //!public folder
 
         this.app.use(express.static('public'))
+
+        this.app.get('*',(req,res)=>{
+            const indexPath = path.join(__dirname + '../../../public/index.html')
+            res.sendFile(indexPath)
+            return
+        })
 
 
         this.app.listen(3000,()=>{
