947fe26c1d57c7404f4b52b12d89a3273a89638a
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-26 22:49:23 +0100

    Rest Server postman

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,42 +1,50 @@
-import express from 'express'
-import path from 'path'
+import express from "express";
+import path from "path";
 
 interface Options {
-    port: number
-    public_path?: string
+  port: number;
+  public_path?: string;
 }
 
-export class Server{
+export class Server {
+  private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
 
-    private app = express()
-    private readonly port: number
-    private readonly publicPath: string
+  constructor(options: Options) {
+    const { port, public_path = "public" } = options;
+    this.port = port;
+    this.publicPath = public_path;
+  }
 
-    constructor(options:Options){
-        const {port, public_path = 'public'}= options
-        this.port = port
-        this.publicPath = public_path
-    }
+  async start() {
+    //!Middlewares
 
-    async start(){
-        
-        //!Middlewares
+    //!public folder
 
-        //!public folder
+    this.app.use(express.static(this.publicPath));
 
-        this.app.use(express.static(this.publicPath))
+    //!Routes
 
-        this.app.get('*',(req,res)=>{
-            const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`)
-            res.sendFile(indexPath)
-            return
-        })
+    this.app.get("/api/todos", (req, res) => {
+      res.json([
+        { id: 1, text: "Buy milk", createdAt: new Date() },
+        { id: 1, text: "Buy bread", createdAt: null },
+        { id: 1, text: "Buy butter", createdAt: new Date() }
+    ]);
+    });
 
+    //*SPA
+    this.app.get("*", (req, res) => {
+      const indexPath = path.join(
+        __dirname + `../../../${this.publicPath}/index.html`
+      );
+      res.sendFile(indexPath);
+      return;
+    });
 
-        this.app.listen(this.port,()=>{
-            console.log(`Server running on port ${this.port}`);
-            
-        })
-        
-    }
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
 }
