44cb9ed575034a469c661a860e8a7ac3778f880f
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-02-13 04:41:26 +0100

    create datasource and repositories

---
+++ src/domain/datasources/todo.datasource.ts
@@ -0,0 +1,16 @@
+import { CreateTodoDTO, UpdateTodoDTO } from "../dtos";
+import { TodoEntity } from "../entities/todo.entity";
+
+
+export abstract class TodoDatasource {
+
+    abstract create(createTodoDto: CreateTodoDTO): Promise<TodoEntity>
+
+    //Todo: paginacion 
+    abstract getAll(): Promise<TodoEntity[]>
+
+    abstract findById(id: number): Promise<TodoEntity>
+    abstract updateTodo(updateTodoDto: UpdateTodoDTO): Promise<TodoEntity>
+    abstract deleteTodo(id: number): Promise<TodoEntity>
+
+}
